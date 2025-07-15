b6af419455b0182975558000798d72aef9d95ef3
Dhara-3007 <sudradhara@gmail.com>
2025-07-15 09:13:29 +0000

    add comparison.js, datatypes_summary.js and modified conversionOperations.js

--- JS/conversionOperations.js
+++ JS/conversionOperations.js
@@ -8,18 +8,59 @@
 // "22abc" - NaN
 // true - 1 false - 0
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLogg = Boolean(isLoggedIn)
-console.log(booleanIsLogg);
+// let booleanIsLogg = Boolean(isLoggedIn)
+// console.log(booleanIsLogg);
 
 // 1 - true 0 - false
 //"" - false
 // "Dhara" - true 
 
-let someNum = 33
+// let someNum = 33
 
-let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// ********** OPERATIONS *************
+
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(2**3);
+// console.log(3%3);
+
+// let str1 = "Dharaa"
+// let str2 = " Sudra"
+
+// let str3 = str1+str2;
+// console.log(str3);
+// console.log((1+4)+"7");
+// console.log(1+4);
+// console.log("1"+4);
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 20;
+// gameCounter++;
+
+// console.log(gameCounter);
+
+// let a = 3;
+// --a;
+// console.log(a);
+
+
+
+
+
+
+
 
---
+++ JS/datatypes_summary.js
@@ -0,0 +1,7 @@
+// Primitive - 7 types
+//string, number, boolean, null, undefined, bigint, symbol
+
+
+
+// Refrence types (Non primitive)
+// arrays, objects, functions,
